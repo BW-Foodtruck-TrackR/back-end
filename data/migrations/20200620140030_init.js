13a7088438e65b8815b7a5f68c317a1720b4b048
Michael Perez <mperez6473@gmail.com>
2020-06-22 12:01:20 -0500

    Rolledback migrations to add a fav trucks section to users and a usertype table

--- data/migrations/20200620140030_init.js
+++ data/migrations/20200620140030_init.js
@@ -1,10 +1,25 @@
 exports.up = function (knex) {
     return knex.schema
+        .createTable("userType", (tbl) => {
+            tbl.increments();
+            tbl.integer("userType").notNullable();
+        })
         .createTable("Users", (tbl) => {
             tbl.increments();
             tbl.string("username", 128).notNullable().unique().index();
             tbl.string("password", 256).notNullable();
             tbl.string("currentLocation");
+            tbl.integer("userOrOperator")
+                .unsigned()
+                .references("userType.id")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
+
+            tbl.integer("favoriteTrucks")
+                .unsigned()
+                .references("TruckOperators.id")
+                .onDelete("RESTRICT")
+                .onUpdate("CASCADE");
         })
         .createTable("TruckOperators", (tbl) => {
             tbl.increments();

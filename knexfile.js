4282d6eafa9147d649fd5dea1a8d750700eb94c7
Michael Perez <mperez6473@gmail.com>
2020-06-22 13:32:54 -0500

    Changed to sqlite all across the board.

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const pgConnection =
-    process.env.DATABASE_URL || "postgresql://postgres@localhost/foodTruck";
+    process.env.DATABASE_URL ||
+    "postgres://pepijvuitjtsey:e61725feb066550881b31730d26d6fc82cdfc035bee9ce1fb58a715c66ff4511@ec2-34-233-226-84.compute-1.amazonaws.com:5432/d3h24ilhojc555";
 module.exports = {
     development: {
         client: "sqlite3",
@@ -38,11 +39,10 @@ module.exports = {
     },
 
     production: {
-        client: "pg",
-        connection: pgConnection,
-        pool: {
-            min: 2,
-            max: 10,
+        client: "sqlite3",
+        useNullAsDefault: true, // needed for sqlite
+        connection: {
+            filename: "./data/foodTruckDev.db3",
         },
         migrations: {
             directory: "./data/migrations",
@@ -50,5 +50,23 @@ module.exports = {
         seeds: {
             directory: "./data/seeds",
         },
+        pool: {
+            afterCreate: (conn, done) => {
+                // runs after a connection is made to the sqlite engine
+                conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
+            },
+        },
+        // client: "pg",
+        // connection: pgConnection,
+        // pool: {
+        //     min: 2,
+        //     max: 10,
+        // },
+        // migrations: {
+        //     directory: "./data/migrations",
+        // },
+        // seeds: {
+        //     directory: "./data/seeds",
+        // },
     },
 };

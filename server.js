2ddab93c010ad0e85296de53aedd69e385e6037f
Michael Perez <mperez6473@gmail.com>
2020-06-22 21:13:53 -0500

    Added package.json, index, and server again

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.status(200).json({
+        Message:
+            "Congrats on loading up the home slash of the server. If you are a developer, you may be looking for /api/login",
+    });
+});
+
+module.exports = server;

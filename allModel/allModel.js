eb7e8937d461586913c57dff2889fcb40b3b1615
Michael Perez <mperez6473@gmail.com>
2020-06-23 10:09:05 -0500

    Have auth router, users router, a temp model, config, new migration and seeds

---
+++ allModel/allModel.js
@@ -0,0 +1,34 @@
+const db = require("../data/connection");
+module.exports = {
+    getUsers,
+    findUser,
+    findByUser,
+    addUser,
+    findByUserId,
+};
+
+function getUsers() {
+    return db("users");
+}
+
+function findUser() {
+    return db("users").select("id", "username").orderBy("id");
+}
+
+function findByUser(filter) {
+    return db("users").where(filter).orderBy("id");
+}
+
+async function addUser(user) {
+    try {
+        const [id] = await db("users").insert(user, "id");
+
+        return findByUserId(id);
+    } catch (error) {
+        throw error;
+    }
+}
+
+function findByUserId(id) {
+    return db("users").where({ id }).first();
+}
---
+++ auth/authRouter.js
@@ -0,0 +1,86 @@
+const router = require("express").Router();
+const bcryptjs = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const randomConsts = require("../config/randomConsts");
+
+const db = require("../allModel/allModel");
+
+function isValid(user) {
+    return Boolean(
+        user.username && user.password && typeof user.password === "string"
+    );
+}
+
+router.post("/register", (req, res) => {
+    const { username, password, userType } = req.body;
+
+    //  hash user password
+    const rounds = process.env.HASH_ROUNDS || 8;
+    const hash = bcryptjs.hashSync(password, rounds);
+    db.addUser({ username, password: hash, userType })
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => res.status(500).json({ error: err.detail, err }));
+});
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+    console.log(username);
+    if (isValid(req.body)) {
+        db.findByUser({ username })
+            .then(([user]) => {
+                // compare the password the hash stored in the database
+                console.log(user);
+                if (user && bcryptjs.compareSync(password, user.password)) {
+                    const token = createToken(user);
+
+                    res.status(200).json({
+                        message: "Welcome to our API",
+                        user: user,
+                        session: req.session,
+                        token,
+                    });
+                } else {
+                    res.status(401).json({ message: "Invalid credentials" });
+                }
+            })
+            .catch((error) => {
+                res.status(500).json({ message: error.message });
+            });
+    } else {
+        res.status(400).json({
+            message:
+                "please provide username and password and the password shoud be alphanumeric",
+        });
+    }
+});
+
+router.get("/logout", (req, res) => {
+    if (req.session) {
+        req.session.destroy((err) => {
+            if (err) {
+                console.log(err);
+                res.status(500).json({
+                    Error: "Could not log out, please try again...",
+                });
+            } else res.status(204).end();
+        });
+    } else {
+        req.status(204).end();
+    }
+});
+function createToken(user) {
+    const payload = {
+        subject: user.id,
+        username: user.username,
+        userType: user.userType,
+    };
+    const secret = randomConsts.jwtSecret;
+    const options = {
+        expiresIn: "30m",
+    };
+    return jwt.sign(payload, secret, options);
+}
+
+module.exports = router;
